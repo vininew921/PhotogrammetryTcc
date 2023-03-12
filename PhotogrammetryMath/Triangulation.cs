e55b262e3c8858501fccd5a52b343919776b3ac0
Vinicius Barbosa <vininew921@hotmail.com>
2023-03-11 21:27:19 -0300

    Fixed GetZ order of operation

--- PhotogrammetryMath/Triangulation.cs
+++ PhotogrammetryMath/Triangulation.cs
@@ -2,21 +2,21 @@
 
 public static class Triangulation
 {
-    public static float GetZ(float cameraConstant, float b, float x1, float x2)
-    {
-        float z = cameraConstant * (b / -(x1 - x2));
-        return z;
-    }
-
     public static float GetX(float b, float x1, float x2)
     {
-        float x = x1 * (b / -(x2 - x1));
+        float x = x1 * (b / -((x2 - x1)));
         return x;
     }
 
     public static float GetY(float y1, float y2, float b, float x1, float x2)
     {
-        float y = y1 * (b / (-(x2 - x1)));
+        float y = ((y1 + y2) / 2) * (b / (-(x2 - x1)));
         return y;
+    }
+
+    public static float GetZ(float cameraConstant, float b, float x1, float x2)
+    {
+        float z = cameraConstant * (b / -((x2 - x1)));
+        return z;
     }
 }
